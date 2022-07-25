ec4706bc31f0f9d2a6d9e9343f4570d197241eed
WBomfim <willianbomfim@hotmail.com>
2022-07-25 03:02:42 -0300

    Style: Corrigida a identação do código do arquivo desafio11.sql

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,6 @@
 SELECT titulo AS nome_musica,
 CASE 
-	WHEN titulo LIKE '%Streets' THEN REPLACE(titulo, 'Streets', 'Code Review')
+	  WHEN titulo LIKE '%Streets' THEN REPLACE(titulo, 'Streets', 'Code Review')
     WHEN titulo LIKE '%Her Own' THEN REPLACE(titulo, 'Her Own', 'Trybe')
     WHEN titulo LIKE '%Inner Fire' THEN REPLACE(titulo, 'Inner Fire', 'Project')
     WHEN titulo LIKE '%Silly' THEN REPLACE(titulo, 'Silly', 'Nice')
